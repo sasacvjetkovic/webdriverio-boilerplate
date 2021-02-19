121bd816a201975c2536f5f60f62d3c354857d00
Saša Cvjetković <sasa.cvjetkovic@parsable.com>
2021-02-19 11:24:56 +0100

    App name extracted into a variable

--- build-ios-debug.sh
+++ build-ios-debug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BUILD_PATH='/Users/'$USER'/Documents/tmp_build'
 APP_PATH='/builds/ios'
-APP_NAME='reactDemo.app'
+APP_NAME='reactDemo'
 DEVICE_UUID=$(xcrun simctl list devices|grep "iPhone 11"|grep -Eo "[0-9A-F\-]{36}"|head -n 1)
 OS=$( xcrun simctl list devices|grep "iOS"|grep -Eo "[0-9\.]{4}"|head -n 1)
 
@@ -28,10 +28,10 @@ echo 'Install pod'
 pod install
 
 echo 'Build .app'
-xcodebuild -workspace "reactDemo.xcworkspace" -scheme "reactDemo" -configuration "Debug" -destination "platform=iOS Simulator,OS=$OS,id=$DEVICE_UUID" -derivedDataPath $BUILD_PATH
+xcodebuild -workspace "$APP_NAME.xcworkspace" -scheme $APP_NAME -configuration "Debug" -destination "platform=iOS Simulator,OS=$OS,id=$DEVICE_UUID" -derivedDataPath $BUILD_PATH
 
 echo 'Copy generated .app file into project structure'
-cp -R $BUILD_PATH'/Build/Products/Debug-iphonesimulator/reactDemo.app' ../$APP_PATH
+cp -R $BUILD_PATH'/Build/Products/Debug-iphonesimulator/'$APP_NAME'.app' ../$APP_PATH
 
 echo 'Remove temporary folder'
 rm -rf $BUILD_PATH
